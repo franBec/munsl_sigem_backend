3021915b3732a3cfeea123c837083c21638b18fe
Fran Bec <franbecvort@gmail.com>
2025-01-01 18:39:53 +0000

    initial commit

---
+++ src/main/groovy/ar/gob/sanluislaciudad/munsl_backend/MunslBackendApplication.groovy
@@ -0,0 +1,13 @@
+package ar.gob.sanluislaciudad.munsl_backend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MunslBackendApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MunslBackendApplication, args)
+	}
+
+}
---
+++ src/test/groovy/ar/gob/sanluislaciudad/munsl_backend/MunslBackendApplicationTests.groovy
@@ -0,0 +1,13 @@
+package ar.gob.sanluislaciudad.munsl_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MunslBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
