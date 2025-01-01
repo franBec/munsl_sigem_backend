06f5bfd424ef1486a59a8492445461195ba21be1
Fran Bec <franbecvort@gmail.com>
2025-01-01 19:00:07 +0000

    first build and run

--- src/main/groovy/ar/gob/sanluislaciudad/munsl_backend/MunslBackendApplication.groovy
+++ src/main/groovy/ar/gob/sanluislaciudad/munsl_backend/MunslBackendApplication.groovy
@@ -9,5 +9,4 @@ class MunslBackendApplication {
 	static void main(String[] args) {
 		SpringApplication.run(MunslBackendApplication, args)
 	}
-
 }
--- src/test/groovy/ar/gob/sanluislaciudad/munsl_backend/MunslBackendApplicationTests.groovy
+++ src/test/groovy/ar/gob/sanluislaciudad/munsl_backend/MunslBackendApplicationTests.groovy
@@ -9,5 +9,4 @@ class MunslBackendApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
