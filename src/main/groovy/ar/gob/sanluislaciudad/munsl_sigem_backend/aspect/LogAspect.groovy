bf104412926f03b788ae2712e116993376c5c149
Fran Bec <franbecvort@gmail.com>
2025-01-07 17:26:59 +0000

    logging

---
+++ src/main/groovy/ar/gob/sanluislaciudad/munsl_sigem_backend/aspect/LogAspect.groovy
@@ -0,0 +1,34 @@
+package ar.gob.sanluislaciudad.munsl_sigem_backend.aspect
+
+import groovy.util.logging.Slf4j
+import org.aspectj.lang.JoinPoint
+import org.aspectj.lang.annotation.AfterReturning
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
+import org.aspectj.lang.annotation.Pointcut
+import org.springframework.stereotype.Component
+
+@Aspect
+@Component
+@Slf4j
+class LogAspect {
+
+	@Pointcut("execution(public * ar.gob.sanluislaciudad.munsl_sigem_backend.controller..*.*(..))")
+	void controllerPublicMethodsPointcut() {
+		// Pointcut for public methods in controller package
+	}
+
+	@Before("controllerPublicMethodsPointcut()")
+	static void logBefore(JoinPoint joinPoint) {
+		log.info(
+				"[${joinPoint.signature.toShortString()}] Args: ${joinPoint.args}"
+				)
+	}
+
+	@AfterReturning(pointcut = "controllerPublicMethodsPointcut()", returning = "result")
+	static void logAfterReturning(JoinPoint joinPoint, Object result) {
+		log.info(
+				"[${joinPoint.signature.toShortString()}] Response: ${result}"
+				)
+	}
+}
---
+++ src/main/groovy/ar/gob/sanluislaciudad/munsl_sigem_backend/filter/LogFilter.groovy
@@ -0,0 +1,40 @@
+package ar.gob.sanluislaciudad.munsl_sigem_backend.filter
+
+import groovy.util.logging.Slf4j
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.stereotype.Component
+
+@Component
+@Slf4j
+class LogFilter implements Filter {
+
+	@Override
+	void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
+		logRequestDetails(servletRequest as HttpServletRequest)
+		filterChain.doFilter(servletRequest, servletResponse)
+		logResponseDetails(servletResponse as HttpServletResponse)
+	}
+
+	private static void logRequestDetails(HttpServletRequest request) {
+		log.info(
+				">>>> Method: ${request.method}; URI: ${request.requestURI}; QueryString: ${request.queryString}; Headers: ${headersToString(request)}"
+				)
+	}
+
+	private static String headersToString(HttpServletRequest request) {
+		List<GString> headers = request.headerNames.collect { headerName ->
+			"${headerName}: ${request.getHeader(headerName)}"
+		}
+		"{${headers.join(', ')}}"
+	}
+
+
+	private static void logResponseDetails(HttpServletResponse response) {
+		log.info("<<<< Response Status: ${response.status}")
+	}
+}
