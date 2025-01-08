e43c39b90d9a371f3cae88fc315ddd51adc8617a
Fran Bec <franbecvort@gmail.com>
2025-01-08 13:42:49 +0000

    sanitization of logs

--- src/main/groovy/ar/gob/sanluislaciudad/munsl_sigem_backend/aspect/LogAspect.groovy
+++ src/main/groovy/ar/gob/sanluislaciudad/munsl_sigem_backend/aspect/LogAspect.groovy
@@ -13,6 +13,9 @@ import org.springframework.stereotype.Component
 @Slf4j
 class LogAspect {
 
+	public static final String SANITIZED_VALUE = '········'
+	private static final List<String> sensitiveFields = ["password"]
+
 	@Pointcut("execution(public * ar.gob.sanluislaciudad.munsl_sigem_backend.controller..*.*(..))")
 	void controllerPublicMethodsPointcut() {
 		// Pointcut for public methods in controller package
@@ -20,8 +23,9 @@ class LogAspect {
 
 	@Before("controllerPublicMethodsPointcut()")
 	static void logBefore(JoinPoint joinPoint) {
+		def sanitizedArgs = sanitizeForLogging(joinPoint.args)
 		log.info(
-				"[${joinPoint.signature.toShortString()}] Args: ${joinPoint.args}"
+				"[${joinPoint.signature.toShortString()}] Args: ${sanitizedArgs}"
 				)
 	}
 
@@ -30,5 +34,38 @@ class LogAspect {
 		log.info(
 				"[${joinPoint.signature.toShortString()}] Response: ${result}"
 				)
+	}
+
+	private static Object sanitizeForLogging(Object[] args) {
+		args.collect { arg ->
+			if (arg instanceof Map) {
+				arg.collectEntries { k, v ->
+					isSensitiveField(k as String) ? [k, SANITIZED_VALUE] : [k, v]
+				}
+			} else if (arg instanceof String || arg instanceof CharSequence) {
+				arg
+			} else {
+				try {
+					def sanitizedCopy = arg.class.getDeclaredConstructor().newInstance()
+					def fields = arg.class.declaredFields
+					fields.each { field ->
+						field.accessible = true
+						def value = field.get(arg)
+						if (isSensitiveField(field.name)) {
+							field.set(sanitizedCopy, SANITIZED_VALUE)
+						} else {
+							field.set(sanitizedCopy, value)
+						}
+					}
+					sanitizedCopy
+				} catch (Exception ignored) {
+					arg
+				}
+			}
+		}
+	}
+
+	private static boolean isSensitiveField(String fieldName) {
+		sensitiveFields.any { it.equalsIgnoreCase(fieldName) }
 	}
 }
